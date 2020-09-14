49089534319e54e3da4fb79a2506c6b8c1c8b802
unknown <juanherreros4@gmail.com>
2020-09-14 18:41:46 +0200

    Simplify API calls, use default user

--- src/@lekoarts/gatsby-theme-minimal-blog/texts/MyComponent.js
+++ src/@lekoarts/gatsby-theme-minimal-blog/texts/MyComponent.js
@@ -1,55 +1,41 @@
 import React, { Component } from "react";
 const axios = require('axios');
-const ipApi = "https://json.geoiplookup.io/api";
-const apiUrl = 'https://o1bo5vy254.execute-api.eu-central-1.amazonaws.com/Prod/count/';
+const apiUrl = 'https://o1bo5vy254.execute-api.eu-central-1.amazonaws.com/Prod/count/default';
 
 export default class MyComponent extends Component {
   state = {
     error: null,
     isLoaded: false,
-    userIp: '0.0.0.0',
     userCount: 0
   };
 
   componentDidMount() {
-    axios.get( ipApi ).then(
+
+    axios.get(apiUrl).then(
       response => {
         this.setState({
-          userIp: response.data.ip
+          isLoaded: true,
+          userCount: response.data.userCount
         });
-        axios.get( apiUrl.concat(this.state.userIp) ).then(
-            response => {
-                this.setState({
-                    isLoaded: true,
-                    userCount: response.data.userCount
-                });
-            },
-            error => {
-                this.setState({
-                    isLoaded: true,
-                    error
-                });
-            }
-        )
       },
       error => {
         this.setState({
-            isLoaded: true,
-            error
+          isLoaded: true,
+          error
         });
       }
-      );
+    )
   }
 
   render() {
-    const { error, isLoaded, userIp, userCount } = this.state;
+    const { error, isLoaded, userCount } = this.state;
     if (error) {
       return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
       return <div>Loading...</div>;
     } else {
       return (
-      <p>You have visited this page {userCount} times from your IP: {userIp}</p>
+        <p>Number of visits: {userCount}</p>
       );
     }
   }
