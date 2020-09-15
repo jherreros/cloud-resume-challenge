d55f96e35b7eb0f94655c9832495bfa858aedfdb
unknown <juanherreros4@gmail.com>
2020-09-15 19:53:20 +0200

    Customize some params

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,11 @@ module.exports = {
   siteMetadata: {
     siteTitle: `Juan Herreros Elorza`,
     siteTitleAlt: `Juan Herreros Elorza - Personal site`,
+    siteHeadline: `Juan Herreros Elorza - Personal site`,
+    siteUrl: `https://juanherreros.com`,
+    siteDescription: `This is my personal web page. Here I host my CV and a blog about the creation of the project.`,
+    siteLanguage: `en`,
+    author: `Juan Herreros Elorza`,
   },
   plugins: [
     {
